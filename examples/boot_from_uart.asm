dabf26e06be3949f6bf1653687316c31e3a4d39f
Axel Olsson <axel.olsson@insurello.se>
2024-11-23 11:29:24 +0100

    dec jcmpz

--- examples/boot_from_uart.asm
+++ examples/boot_from_uart.asm
@@ -1,33 +1,68 @@
 #include "../ruledef.customasm"
 
-; TODO - echo atm
-;transmit_u:
+mem_copy_check:
+    ld a, 0x01
+    ld a, 0x02
+    ld a, 0x04
+    ld a, 0x08
+    ld a, 0x10
+    ld a, 0x20
+    ld a, 0x40
+    ld a, 0x80
+    ld a, 0x40
+    ld a, 0x20
+    ld a, 0x10
+    ld a, 0x08
+    ld a, 0x04
+    ld a, 0x02
+    ld a, 0x01
+    ld a, 0x02
+    ld a, 0x04
+    ld a, 0x08
+    ld a, 0x10
+    ld a, 0x20
+    ld a, 0x40
+    ld a, 0x80
+    ld a, 0x40
+    ld a, 0x20
+    ld a, 0x10
+    ld a, 0x08
+    ld a, 0x04
+    ld a, 0x02
+    ld a, 0x01
 
-;ld i, str_u
-;ld t, [i++]
+ld a, "U"
+ld b, 0x10
+ld c, 0x00
 
-;tx start
-;tx
-;tx
-;tx
-;tx
-;tx
-;tx
-;tx
-;tx
-;tx stop
+transmit_u:
+    ld k, .next
 
-;jmp transmit_u
+    jcmpz c, .next_hi
 
-;str_u: #d "U\0"
+.write:
+    jmp fn_uart_write_char
 
+.next:
+    dec c
+    jnz .write
+
+.next_hi:
+    dec b
+    jc .write
+
+;    dec c
+;    decc b     ; if !CF sub 1 else if ZF do sub b, 0 else nothing
+;    jnz .next
 
 main:
     ld i, str_version
     jal k, fn_uart_write_string
 
+; read a character
+    ld k, .read ; return to .read
+
 .read:
-    ; read a character
     out 0xfd ; enable RTS
 .wait:
     rx start, .got_rx_start_bit
@@ -45,9 +80,8 @@ main:
     rx
     ld a, rx
 
-    jal k, fn_uart_write_char
-
-    jmp .read
+; echo read character, return to k (.read)
+    jmp fn_uart_write_char
 
 
 fn_uart_write_char:
@@ -67,7 +101,7 @@ fn_uart_write_char:
 
 fn_uart_write_string:
     ld a, [i++]
-    jcmpz k, a
+    jcmpz a, k
 
     ld tx, a
     tx
